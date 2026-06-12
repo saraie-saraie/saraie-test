b57aa0afbd25facbaa88a8c72e59e9a217de44b9
saraie-saraie <saraie271828nei@oji-gr.com>
2026-06-12 13:44:41 +0900

    git diff

--- test.c
+++ test.c
@@ -17,6 +17,7 @@ int main() {
     printf("git branch update-venue\n");
     printf("git checkout update-venue\n");
     printf("git push --set-upstream origin update-venue\n");
+    printf("git diff\n");
 }
 
 
