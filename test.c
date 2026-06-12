cca200a43066a8011287190ab7a6cba67ba63998
saraie-saraie <saraie271828nei@oji-gr.com>
2026-06-12 13:18:57 +0900

    test version1.01 branch test

--- test.c
+++ test.c
@@ -8,6 +8,13 @@ int main() {
     int b = a * 2;
     printf("The value of b is: %d\n", b);
     return 0;
+
+    if(a > 10) {
+        printf("a is greater than 10\n");
+    } else {
+        printf("a is less than or equal to 10\n");
+    }
 }
-//test version 1.0 20260612
+//test version 1.01 20260612
+
 
