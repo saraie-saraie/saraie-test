9999995ff16f767844af7a504302323be826a9f1
saraie-saraie <saraie271828nei@oji-gr.com>
2026-06-12 11:45:16 +0900

    saraie test.c

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    
+    printf("Hello, World!\n");
+
+    int a;
+    scanf("%d", &a);
+    int b = a * 2;
+    printf("The value of b is: %d\n", b);
+    return 0;
+}
+//test version 1.0 20260612
+
